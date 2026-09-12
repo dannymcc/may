5935a4a2f79598fce7cdac34612bea8f0e6a69a1
Danny McClelland <danny@dmcc.io>
2026-09-12 21:06:54 +0100

    chore: prepare v0.44.2 release

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ basedir = Path(__file__).parent.absolute()
 load_dotenv(basedir / '.env')
 
 
-APP_VERSION = '0.44.1'
+APP_VERSION = '0.44.2'
 RELEASE_CHANNEL = os.environ.get('RELEASE_CHANNEL', 'stable')
 GIT_SHA = os.environ.get('GIT_SHA', '')[:7]  # Short SHA
 GITHUB_REPO = 'dannymcc/may'
